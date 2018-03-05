47baf5cbf11f0de5603e2d656fae46c3f60d1a85
Bobby Caya <bob.caya1@gmail.com>
2018-03-05 11:42:42 -0700

    add form to show

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -1,2 +1,63 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+//Game state
+var currentGame = {}
+var showForm = false
+
+//Helper functions
+function toggleForm() {
+  showForm = !showForm;
+  $('#game-form').remove();
+  $('#games-list').toggle();
+
+  if (showForm) {
+    $.ajax({
+      url: '/game_form',
+      method: 'GET'
+    }).done( function(html) {
+      $('#toggle').after(html)
+    });
+  }
+}
+
+$(document).ready( function() {
+  //Form submit handler
+  $(document).on('submit', '#game-form form', function(e) {
+    e.preventDefault();
+    toggleForm();
+    var data = $(this).serializeArray();
+    $.ajax({
+      url: '/games',
+      type: 'POST',
+      dataType: 'JSON',
+      data: data
+    }).done( function(game) {
+      var g = '<li class="game-item" data-id="' + game.id + '" data-name="' + data.name + '">' + game.name + '-' + game.description + '</li>';
+      $('#games-list').append(g);
+    }).fail( function(err) {
+      alert(err.responseJSON.errors)
+    });
+  });
+
+  //Toggle form click handler
+  $('#toggle').on('click', function() {
+    toggleForm()
+  });
+
+  //Game select click handler
+  $(document).on('click', '.game-item', function() {
+    currentGame.id = this.dataset.id;
+    currentGame.name = this.dataset.name;
+    $.ajax({
+      url: '/games/' + currentGame.id + '/characters',
+      type: 'GET',
+      dataType: 'JSON'
+    }).done( function(characters) {
+      $('#game').text('Characters in ' + currentGame.name);
+      var list = $('#characters');
+      list.empty();
+      characters.forEach( function(char) {
+        var li = '<li data-character-id="' + char.id + '">' + char.name + ' - ' + char.power + '</li>'
+        list.append(li)
+      });
+    });
+  });
+});
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def render_error(model)
+    render json: { errors: model.errors.full_messages.join(',') }, status: 422
 end
+end 
